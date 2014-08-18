3049dffaf13c8919bcaf272f5f82f641c31eac40
nirmalz <nirmal_thapa_web@hotmail.com>
2014-08-18 18:45:03 +0545

    inserted /post route to insert post

--- app/routes.php
+++ app/routes.php
@@ -18,7 +18,7 @@ Route::get('/', function()
 
 Route::get('/user', function(){
 	$user = new User;
-	$user->username 				= 'Harry';
+	$user->username 				= '';
 	$user->email 					= 'harry@gmail.com';
 	$user->password 				= 'general';
 	$user->password_confirmation 	= 'general';
@@ -30,6 +30,27 @@ Route::get('/user', function(){
 		echo "Make sure you did everything right !!";
 
 		foreach ($user->errors()->all() as $error) {
+			echo "<div>$error</div>"; 
+		}
+	}
+
+});
+
+Route::get('/post', function(){
+	
+	//create a new Post
+	$post = new Post(array('body' => 'Binog, The hird post is now live.'));
+
+	//grab User 1
+	$user = User::find(1);
+
+	//save the post
+	if($user->posts()->save($post)){
+		echo "User successfully inserted into users table";
+	}else{
+		echo "Make sure You have written the body content !!";
+
+		foreach ($post->errors()->all() as $error) {
 			echo "<div>$error</div>"; 
 		}
 	}
