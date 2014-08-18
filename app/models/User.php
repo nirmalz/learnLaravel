5e95d15d3f571af69a23ea08f7cce7440d2e5ab8
nirmalz <nirmal_thapa_web@hotmail.com>
2014-08-18 11:33:36 +0545

    Updated User model to make use of Ardent for validation purpose

--- app/models/User.php
+++ app/models/User.php
@@ -4,6 +4,7 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+
 use LaravelBook\Ardent\Ardent;
 
 class User extends Ardent implements UserInterface, RemindableInterface {
@@ -13,12 +14,32 @@ class User extends Ardent implements UserInterface, RemindableInterface {
 	protected $table = 'users';
 	protected $hidden = array('password', 'remember_token');
 
+
+	//Purge Redundant data
+	public $autoPurgeRedundantAttributes = true;
+
+	//password hasing
+	public static $passwordAttributes  = array('password');
+  	public $autoHashPasswordAttributes = true;
+
 	// Validation rules
-	private $rules = array(
+	public static $rules = array(
 		'username'				=> 'required|min:3|max:80|alpha_dash|unique:users',
 		'email'					=> 'required|between:3,64|email|unique:users',
 		'password'				=> 'required|alpha_num|between:4,15|confirmed',
 		'password_confirmation' => 'required|alpha_num|between:4,15'
 		);
+
+	//custom validation message
+	public static $customMessages = array(
+		'required'		=> 'The :attribute is required.'	
+		);
+
+	//relationship definitions
+	public static $relationsData = array(
+
+		);
+
+
 
 }
